dffe1b5f93fa16464c609ced39cb7e01d867a987
Favour Ukonu <favouroukonu@gmail.com>
2024-11-26 22:07:33 +0100

    Chore: Furniro

---
+++ src/app/Homepage/page.tsx
@@ -0,0 +1,13 @@
+import NavBar from '@/components/NavBar'
+import React from 'react'
+
+const Home = () => {
+  return (
+   <section>
+    <NavBar/>
+    <h1>Welcome to my home page</h1>
+   </section>
+  )
+}
+
+export default Home
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "var(--background), #fff, #F4F5F7, #E97171, #2EC1AC, #D8D8D8, ",
         foreground: "var(--foreground)",
+        primaryBrown: '#B88E2F, #FFF3E3, #FCF8F3, #FAF3EA, #F9F1E7,  ',
+        primaryText: '#000, #242424, #333333,  #666666, #3A3A3A, #898989, #B0B0B0, #616161, #C4C4C4, #9F9F9F, #D9D9D9 ',
+
       },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      }
     },
   },
   plugins: [],
