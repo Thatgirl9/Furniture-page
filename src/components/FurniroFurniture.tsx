948f5d5740858b093685ffe12ceac07e95f6cf50
Favour Ukonu <favouroukonu@gmail.com>
2025-01-07 22:05:15 +0100

    Ft: Mobile responsiveness chore

--- src/components/FurniroFurniture.tsx
+++ src/components/FurniroFurniture.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import FurnitureImage1 from "@/assets/homePage/Rectangle 36.png";
 import FurnitureImage2 from "@/assets/homePage/Rectangle 37.png";
@@ -32,32 +32,48 @@ const FurniroFurniture = () => {
         #FurniroFurniture
       </h1>
       <div className="flex w-full gap-4 mt-4">
-
         <div>
           <div className="flex items-end gap-4 mb-4">
             <Image
               src={images[0]}
               alt="Image 1"
-              className="w-[100px] h-[380px]" loading="lazy" placeholder="blur"
+              className="w-[100px] h-[380px]"
+              loading="lazy"
+              placeholder="blur"
             />
             <Image
               src={images[2]}
               alt="Image 3"
-              className="w-[390px] h-[300px]" loading="lazy" placeholder="blur"
+              className="w-[390px] h-[300px]"
+              loading="lazy"
+              placeholder="blur"
             />
           </div>
           <div className="flex gap-4">
-            <Image src={images[1]} alt="Image 2" className="w-[150px] h-auto" loading="lazy" placeholder="blur" />
+            <Image
+              src={images[1]}
+              alt="Image 2"
+              className="w-[150px] h-auto"
+              loading="lazy"
+              placeholder="blur"
+            />
             <Image
               src={images[3]}
               alt="Image 4"
               className="w-[322px] h-[200px]"
-              loading="lazy" placeholder="blur"
+              loading="lazy"
+              placeholder="blur"
             />
           </div>
         </div>
         <div className="flex items-center gap-4">
-          <Image src={images[4]} alt="Image 5" className="w-full h-[392px]" loading="lazy" placeholder="blur" />
+          <Image
+            src={images[4]}
+            alt="Image 5"
+            className="w-full h-[392px]"
+            loading="lazy"
+            placeholder="blur"
+          />
         </div>
         <div>
           <div className="flex gap-4 items-end">
@@ -65,13 +81,15 @@ const FurniroFurniture = () => {
               src={images[6]}
               alt="Image 6"
               className="w-[230px] h-[300px]"
-              loading="lazy" placeholder="blur"
+              loading="lazy"
+              placeholder="blur"
             />
             <Image
               src={images[8]}
               alt="Image 7"
               className="w-[230px] h-[380px]"
-              loading="lazy" placeholder="blur"
+              loading="lazy"
+              placeholder="blur"
             />
           </div>
           <div className="flex gap-4 mt-4">
@@ -79,13 +97,15 @@ const FurniroFurniture = () => {
               src={images[5]}
               alt="Image 8"
               className="w-[150px] h-[242px]"
-              loading="lazy" placeholder="blur"
+              loading="lazy"
+              placeholder="blur"
             />
             <Image
               src={images[7]}
               alt="Image 6"
               className="w-[240px] h-[185px]"
-              loading="lazy" placeholder="blur"
+              loading="lazy"
+              placeholder="blur"
             />
           </div>
         </div>
